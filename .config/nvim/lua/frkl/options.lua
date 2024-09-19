9641890e5c89de195162382dec633ebef7cb2357
Frklcedo <franklinmacedo5115@gmail.com>
2024-09-19 02:08:28 -0300

    new zsh config

--- .config/nvim/lua/frkl/options.lua
+++ .config/nvim/lua/frkl/options.lua
@@ -35,7 +35,7 @@ local options = {
 	linebreak = true, -- companion to wrap, don't split words
 	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
 	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-	guifont = "UbuntuMono Nerd Font Mono:h12", -- the font used in graphical neovim applications
+	guifont = "FiraCode Nerd Font Mono:h10", -- the font used in graphical neovim applications
 	whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
