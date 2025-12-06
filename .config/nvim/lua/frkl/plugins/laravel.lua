0d65c7644c1dd302e0a017fe2845027f22e995db
Frklcedo <franklinmacedo5115@gmail.com>
2025-12-06 17:54:46 -0300

    neovim: Laravel plugin

--- .config/nvim/lua/frkl/plugins/laravel.lua
+++ .config/nvim/lua/frkl/plugins/laravel.lua
@@ -6,8 +6,8 @@ return {
             "nvim-lua/plenary.nvim",
         },
         keys = {
-            { "<leader>la", ":Artisan<cr>",      desc = "Laravel Artisan" },
-            { "<leader>lc", ":Composer",     desc = "Composer" },
+            { "<leader>la", ":Artisan ",      desc = "Laravel Artisan" },
+            { "<leader>lc", ":Composer ",     desc = "Composer" },
             { "<leader>lr", ":LaravelRoute<cr>", desc = "Laravel Routes" },
             { "<leader>lm", ":LaravelMake<cr>",  desc = "Laravel Make" },
         },
