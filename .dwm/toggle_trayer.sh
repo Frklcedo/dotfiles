8b0645f4cf42f77d7c011a2ae6d968256534818a
Frklcedo <franklinmacedo5115@gmail.com>
2026-01-06 12:34:41 -0300

    trayer and tmux

--- .dwm/toggle_trayer.sh
+++ .dwm/toggle_trayer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TRAYER="trayer --edge bottom --iconspacing 2 --height 20 --align center --padding 20 --expand false --width 50"
+TRAYER="trayer --edge top --align right --width 75 --height 20 --expand true --padding 12 --iconspacing 2 --transparent true --alpha 0 --tint 0x1e2127"
 
 if [ $(pgrep -x trayer) ]; then
     pkill -USR1 trayer
